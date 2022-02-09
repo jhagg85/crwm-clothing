90fe520adc4402f1f500fe95e7bfadef14428ae2
Joseph Garganera <josephgarganera@Josephs-MacBook-Pro.local>
2022-02-09 10:46:47 +0800

    breakdown homepage into smaller components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import Homepage from "./Homepage/Homepage";
+import Homepage from "./Pages/Homepage/Homepage";
 
 function App() {
   return (
---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Homepage.scss";
+
+import Directory from "../../Components/directory/directory";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
